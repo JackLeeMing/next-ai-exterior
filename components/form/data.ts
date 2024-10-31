18b6202d3365174f12f24fa4f5ae4351d88ebcac
JiaKuiLee <JackLeeMing@outlook.com>
2024-10-31 17:29:52 +0800

    fix form create

---
+++ components/form/data.ts
@@ -0,0 +1,38 @@
+export const testdata: any = {
+    "id": "4d9hwh45h5rj60cjw7cs9zf8ag",
+    "model": "jagilley/controlnet-hough",
+    "version": "854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b",
+    "input": {
+        "a_prompt": "best quality, extremely detailed",
+        "ddim_steps": 20,
+        "detect_resolution": 512,
+        "distance_threshold": 0.1,
+        "eta": 0,
+        "image": "data:image/webp;base64,...",
+        "image_resolution": "512",
+        "n_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
+        "num_samples": "1",
+        "prompt": "exterior-house-remodel, Townhouse style",
+        "scale": 9,
+        "value_threshold": 0.1
+    },
+    "logs": "Global seed set to 34655\nGlobal seed set to 34655\nData shape for DDIM sampling is (1, 4, 64, 80), eta 0.0\nRunning DDIM Sampling with 20 timesteps\nDDIM Sampler:   0%|          | 0/20 [00:00<?, ?it/s]\nDDIM Sampler:   5%|▌         | 1/20 [00:00<00:03,  5.16it/s]\nDDIM Sampler:  10%|█         | 2/20 [00:00<00:03,  5.18it/s]\nDDIM Sampler:  15%|█▌        | 3/20 [00:00<00:03,  5.19it/s]\nDDIM Sampler:  20%|██        | 4/20 [00:00<00:03,  5.19it/s]\nDDIM Sampler:  25%|██▌       | 5/20 [00:00<00:02,  5.20it/s]\nDDIM Sampler:  30%|███       | 6/20 [00:01<00:02,  5.19it/s]\nDDIM Sampler:  35%|███▌      | 7/20 [00:01<00:02,  5.19it/s]\nDDIM Sampler:  40%|████      | 8/20 [00:01<00:02,  5.19it/s]\nDDIM Sampler:  45%|████▌     | 9/20 [00:01<00:02,  5.19it/s]\nDDIM Sampler:  50%|█████     | 10/20 [00:01<00:01,  5.19it/s]\nDDIM Sampler:  55%|█████▌    | 11/20 [00:02<00:01,  5.19it/s]\nDDIM Sampler:  60%|██████    | 12/20 [00:02<00:01,  5.19it/s]\nDDIM Sampler:  65%|██████▌   | 13/20 [00:02<00:01,  5.19it/s]\nDDIM Sampler:  70%|███████   | 14/20 [00:02<00:01,  5.19it/s]\nDDIM Sampler:  75%|███████▌  | 15/20 [00:02<00:00,  5.20it/s]\nDDIM Sampler:  80%|████████  | 16/20 [00:03<00:00,  5.20it/s]\nDDIM Sampler:  85%|████████▌ | 17/20 [00:03<00:00,  5.20it/s]\nDDIM Sampler:  90%|█████████ | 18/20 [00:03<00:00,  5.19it/s]\nDDIM Sampler:  95%|█████████▌| 19/20 [00:03<00:00,  5.19it/s]\nDDIM Sampler: 100%|██████████| 20/20 [00:03<00:00,  5.20it/s]\nDDIM Sampler: 100%|██████████| 20/20 [00:03<00:00,  5.19it/s]\n",
+    "output": [
+        "https://replicate.delivery/yhqm/xaruXyR8JSIlM9xDCDgFsAMey1f41wLqfVglUlZsAHyEhfwOB/output_0.png",
+        "https://replicate.delivery/yhqm/d3YmOPr8zM4AK1tGzfFO3MwAbUBVKduGeNDyriZdDSViwPsTA/output_1.png"
+    ],
+    "data_removed": false,
+    "error": null,
+    "status": "succeeded",
+    "created_at": "2024-10-31T09:13:02.089Z",
+    "started_at": "2024-10-31T09:13:02.113742407Z",
+    "completed_at": "2024-10-31T09:13:06.252152529Z",
+    "urls": {
+        "cancel": "https://api.replicate.com/v1/predictions/4d9hwh45h5rj60cjw7cs9zf8ag/cancel",
+        "get": "https://api.replicate.com/v1/predictions/4d9hwh45h5rj60cjw7cs9zf8ag",
+        "stream": "https://stream.replicate.com/v1/files/qoxq-ayuasuf2nw6yyg3mvlwakpaj3trbhvjkmzbzaexkmo4h7kzxwc6a"
+    },
+    "metrics": {
+        "predict_time": 4.138410211
+    }
+}
